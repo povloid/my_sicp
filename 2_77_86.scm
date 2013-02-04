4207f35f9719da1f1e592692977a2f61aca1df7c
povloid <povloid@proton-m.(none)>
2013-02-05 01:02:42 +0600

    next

--- 2_77_86.scm
+++ 2_77_86.scm
@@ -99,7 +99,7 @@
   (define (exp x y) (apply-generic 'exp x y))
   (put 'exp '(scheme-number scheme-number)
        (lambda (x y) (tag (expt x y))))
-
+  
   
   
   
@@ -446,30 +446,43 @@
 
 
 
-(define (apply-generic op . args)
+(define (apply-generic op . args)  
   (display "*->")
+  ;; в)
+  (define (apply-generic-old op x y) 
+    ((get op (list (car x) (car y))) 
+     (cdr x) (cdr y)))
+  (define (is-all-type-tags-eq? args)
+    (if (null? args) #t
+        (let ((a (eq? (car args) (cadr args))))
+          (if a 
+              (and a (is-all-type-tags-eq? (cddr args))) #f))))
+  
+  
   (let ((type-tags (map type-tag args)))
-    (display type-tags)
-    (let ((proc (get op type-tags)))
-      (display proc)
-      (if proc
-          (apply proc (map contents args))
-          (if (= (length args) 2)
-              (let ((type1 (car type-tags))
-                    (type2 (cadr type-tags))
-                    (a1 (car args))
-                    (a2 (cadr args)))
-                (let ((t1->t2 (get-coercion type1 type2))
-                      (t2->t1 (get-coercion type2 type1)))                  
-                  (cond (t1->t2
-                         (apply-generic op (t1->t2 a1) a2))
-                        (t2->t1
-                         (apply-generic op a1 (t2->t1 a2)))
-                        (else
-                         (error "No method for these types"
-                                (list op type-tags))))))
-              (error "No method for these types"
-                     (list op type-tags)))))))
+    (display args)
+    ;; в)
+    (if (is-all-type-tags-eq? type-tags) (apply (get op type-tags) (map contents args))
+        
+        (let ((proc (get op type-tags)))
+          (if proc 
+              (apply proc (map contents args))
+              (if (= (length args) 2)
+                  (let ((type1 (car type-tags))
+                        (type2 (cadr type-tags))
+                        (a1 (car args))
+                        (a2 (cadr args)))
+                    (let ((t1->t2 (get-coercion type1 type2))
+                          (t2->t1 (get-coercion type2 type1)))                  
+                      (cond (t1->t2
+                             (apply-generic op (t1->t2 a1) a2))
+                            (t2->t1
+                             (apply-generic op a1 (t2->t1 a2)))
+                            (else
+                             (error "No method for these types"
+                                    (list op type-tags))))))
+                  (error "No method for these types"
+                         (list op type-tags))))))))
 
 
 
@@ -511,6 +524,14 @@
 ; если не определены то происходит зацикливание
 
 ; б) Особого смысла в доработке нет
+
+
+; в) Процедура доработана выше
+
+
+
+
+
 
 
 
