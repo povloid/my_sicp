9d4922b3457ab8498ba97f46044c3cbdd74c9a6b
povloid <t34box@gmail.com>
2013-04-26 00:13:53 +0600

    Выполнил Упражнение 2.89

--- 2_87_X.scm
+++ 2_87_X.scm
@@ -1,3 +1,19 @@
+;; Печать n раз
+               
+(define (print-c-n c n)
+  (newline)
+  (define (cr-list ct nt)
+    (display c)
+    (if (= nt 1)
+        '()
+        (cons ct (cr-list ct (- nt 1)))))
+  (cr-list c n)
+  (newline)(newline))
+
+(define (pline) (print-c-n '- 20))
+
+(pline)
+
 ;; Ошибка
 (define (error message x)
   (newline)(display "Error: ")
@@ -48,8 +64,6 @@
 (define (attach-tag type-tag contents)
   (cons type-tag contents))
 
-
-
 ;; ----------------------------------------------------------
 ; Пустой список термов
 (define
@@ -94,6 +108,84 @@
 (=zero? (make-term 1 0))
 (=zero? (make-term 1 (make-term 1 0)))
 
+
+; Функция определяет максимальный элемент в списке
+(define (max-order-element element tlist)
+    (if (null? tlist)
+        element
+        (max-order-element
+         (if (> element (car tlist))
+             element (car tlist))
+         (cdr tlist))))
+
+(max-order-element 10 (list 10 20 40 20 400 8000 20))
+(newline)
+
+
+
+;; сортировка пузырьковая типовая обобщенная
+(define (sort-1 list1 f fmax fmin)
+  (define (max x y)
+    (if (fmax x y) x y))
+  (define (min x y)
+    (if (fmin x y) x y))
+  (define (test x y)
+    (f x y))
+  (define (split-list tst flist e elist)
+    (cond ((null? elist) (list tst (append flist (list e))))
+          (else (split-list (and tst (test e (car elist)))
+                            (append flist (list (max e (car elist))))
+                            (min e (car elist))
+                            (cdr elist)))))
+  (let ((a (split-list #t '() (car list1) (cdr list1))))
+    (if (car a)
+        (cadr a)
+        (sort-1 (cadr a) f fmax fmin))))
+             
+          
+      
+      
+
+
+
+
+(display '<)
+(sort-1 (list 10 20 30 10 40 60 10 20) 
+        (lambda (x y)(>= x y)) > <)
+
+;(sort-1 (list 10 20 40 20 400 8000 20))
+(display '>)
+
+
+
+
+
+(pline)
+(define terms1 (list (make-term 200 5) (make-term 4 5) (make-term 10 3) (make-term 100 5)))
+
+(display terms1)
+
+
+;; Упражнение 2.89
+(define (sort-terms terms) 
+  (sort-1 terms 
+          (lambda (x y) (>= (order x) (order y)))
+          (lambda (x y) (> (order x) (order y)))
+          (lambda (x y) (< (order x) (order y)))))
+
+
+(define terms2 (sort-terms terms1))
+
+
+
+(newline)
+
+(display terms2)
+
+
+
+(pline)
+
 ; Добавить к списку многочленов новый многочлен
 (define (adjoin-term term term-list)
   (if (=zero? (coeff term))
@@ -137,8 +229,9 @@
       (let ((t2 (first-term L)))
         (adjoin-term
          (make-term (+ (order t1) (order t2))
-                    (mul (coeff t1) (coeff t2)))
+                    (* (coeff t1) (coeff t2)))
          (mul-term-by-all-terms t1 (rest-terms L))))))
+
 
          
         
@@ -152,6 +245,10 @@
   (define (variable p) (car p))
   ;; Получить список термов
   (define (term-list p) (cdr p))
+  
+  
+  (define (same-variable? v1 v2)
+    (eq? v1 v2))
 
   ;; представление термов и списков термов
   ;; (+) Сложение многочленов
@@ -162,6 +259,21 @@
                               (term-list p2)))
         (error "Многочлены от разных переменных -- ADD-POLY"
                (list p1 p2))))
+  
+  
+  ;; (-) Вычитание многочленов (Задание 2.ы88)
+  (define (sub-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly (variable p1)
+                   (add-terms 
+                    ;; По заданию 2.88 меняем знак коэффициентов в одном из многочленов
+                    (map (lambda (x) 
+                           (make-term (order x) (- (coeff x))))
+                     (term-list p1))
+                    (term-list p2)))
+        (error "Многочлены от разных переменных -- ADD-POLY"
+               (list p1 p2))))
+  
   ;; (*) Умножение многочленов
   (define (mul-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
@@ -178,6 +290,9 @@
   ;; Прописываем в системе определенные функции
   (put 'add '(polynomial polynomial)
        (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'sub '(polynomial polynomial)
+       (lambda (p1 p2) (tag (sub-poly p1 p2))))
+  
   (put 'mul '(polynomial polynomial)
        (lambda (p1 p2) (tag (mul-poly p1 p2))))
   (put 'make 'polynomial
@@ -188,11 +303,12 @@
 
 ;; Обобщенные функции 
 (define (apply-generic op x y) 
+  ;(display (cdr x))
   ((get op (list (car x) (car y))) 
    (cdr x) (cdr y)))
 
 (define (add x y) (apply-generic 'add x y))
-;(define (sub x y) (apply-generic 'sub x y))
+(define (sub x y) (apply-generic 'sub x y))
 (define (mul x y) (apply-generic 'mul x y))
 ;(define (div x y) (apply-generic 'div x y))
 
@@ -204,7 +320,24 @@
   ;; Теперь инсталируем пакет в систему
 (install-polynomial-package)
 
-(make (list 'polynomial (list 'x (list 10 2))))
+(define p1 (make (list 'polynomial (list 'x (list 10 2)))))
+
+(define p2 (make (list 'polynomial (list 'x (list 5 4)))))
+
+(pline)
+
+(add p1 p2)
+
+(pline)
+
+(sub p1 p2)
+
+(pline)
+
+(mul p1 p2)
+
+
+
 
 
 
